dd8207a753aaebbac5661bb81a96ebf320f053ef
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-19 12:40:24 +0300

    Brew formula update for bionic version v0.0.4

--- Formula/bionic.rb
+++ Formula/bionic.rb
@@ -5,16 +5,16 @@
 class Bionic < Formula
   desc ""
   homepage "https://bionic.dev"
-  version "0.0.3"
+  version "0.0.4"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/bionic-dev/bionic/releases/download/v0.0.3/bionic_0.0.3_Darwin_x86_64.tar.gz"
-    sha256 "a29a5d14390b51204300c04127eecb97b9b3fcb649d490d1fe5efdb841caf659"
+    url "https://github.com/bionic-dev/bionic/releases/download/v0.0.4/bionic_0.0.4_Darwin_x86_64.tar.gz"
+    sha256 "f403224aa91d494e5945a5ea31a0e2d67ae9648b3c43d7f72e877c69dcf33d3e"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/bionic-dev/bionic/releases/download/v0.0.3/bionic_0.0.3_Linux_x86_64.tar.gz"
-    sha256 "ae2cfb34b2fda69fc5a4fc125dd3fd130671c374d085458fc79889d36b7ab110"
+    url "https://github.com/bionic-dev/bionic/releases/download/v0.0.4/bionic_0.0.4_Linux_x86_64.tar.gz"
+    sha256 "edccf062a5aeca5d0d37e6e37803960788ae0fa8a7301e4f56d49d55b133f0a8"
   end
 
   def install
